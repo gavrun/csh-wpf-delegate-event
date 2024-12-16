1cd0af4d0d7ad1d54877c9a8069bb15ef3027396
gavrun <38539655+gavrun@users.noreply.github.com>
2024-12-16 14:51:09 +0000

    Added project

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace csh_wpf_delegate_event;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
